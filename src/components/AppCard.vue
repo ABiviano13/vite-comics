c44fd8a4db08d1e8ac78eafbf5f9307542013512
ABiviano13 <abiviano0@outlook.com>
2023-03-02 17:59:32 +0100

    FEAT component completed

---
+++ src/components/AppCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    props: {
+        image: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <li class="card">
+        <figure>
+            <img :src="image" alt="">
+        </figure>
+        <div class="text-card">
+            {{ text }}
+        </div>
+    </li>
+
+</template>
+
+<style lang="scss" scoped> 
+
+.card{
+
+    img{
+        width: 180px;
+        aspect-ratio: 1/1;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    .text-card{
+        text-transform: uppercase;
+    }
+}
+ 
+</style>
